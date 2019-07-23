c49ba52e2dd35391d3a097139ec7c5e68dee1cc9
newbone <1106690435@qq.com>
2019-07-23 15:18:24 +0800

    config yml

--- itoken-zipkin/src/main/java/cn/com/itoken/zipkin/ItokenZipkinApplication.java
+++ itoken-zipkin/src/main/java/cn/com/itoken/zipkin/ItokenZipkinApplication.java
@@ -2,8 +2,12 @@ package cn.com.itoken.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
 public class ItokenZipkinApplication {
 
     public static void main(String[] args) {
