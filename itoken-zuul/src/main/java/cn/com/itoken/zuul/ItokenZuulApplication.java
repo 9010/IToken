8fd8c2727f5d4dc85ee9f7a81c8acc57130596b3
newbone <1106690435@qq.com>
2019-07-24 10:41:00 +0800

    change spring cloud version to Greenwich.SR1

--- itoken-zuul/src/main/java/cn/com/itoken/zuul/ItokenZuulApplication.java
+++ itoken-zuul/src/main/java/cn/com/itoken/zuul/ItokenZuulApplication.java
@@ -9,9 +9,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableEurekaClient
 @EnableZuulProxy
 public class ItokenZuulApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ItokenZuulApplication.class, args);
     }
-
 }
