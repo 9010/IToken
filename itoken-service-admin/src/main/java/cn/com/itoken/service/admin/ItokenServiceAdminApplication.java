03c91015047bf0f2189e5228a02f999644cfc890
newbone <1106690435@qq.com>
2019-07-31 11:52:41 +0800

    ini itoken.service.admin

---
+++ itoken-service-admin/src/main/java/cn/com/itoken/service/admin/ItokenServiceAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.com.itoken.service.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenServiceAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenServiceAdminApplication.class, args);
+    }
+
+}
