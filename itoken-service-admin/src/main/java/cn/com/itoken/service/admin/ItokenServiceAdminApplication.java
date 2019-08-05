98562a17beeae342cb48d369216beb7a1a763f19
newbone <1106690435@qq.com>
2019-08-05 14:38:12 +0800

    add SQL.txt

--- itoken-service-admin/src/main/java/cn/com/itoken/service/admin/ItokenServiceAdminApplication.java
+++ itoken-service-admin/src/main/java/cn/com/itoken/service/admin/ItokenServiceAdminApplication.java
@@ -2,8 +2,10 @@ package cn.com.itoken.service.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ItokenServiceAdminApplication {
 
     public static void main(String[] args) {
