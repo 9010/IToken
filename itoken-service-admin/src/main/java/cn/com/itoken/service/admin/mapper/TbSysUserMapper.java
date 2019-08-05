ac160034ef283f1cc6c55a132d60da8ecdd41b03
newbone <1106690435@qq.com>
2019-08-05 16:39:29 +0800

    generate mapper

---
+++ itoken-service-admin/src/main/java/cn/com/itoken/service/admin/mapper/TbSysUserMapper.java
@@ -0,0 +1,7 @@
+package cn.com.itoken.service.admin.mapper;
+
+import cn.com.itoken.common.service.tk.mybatis.mapper.MyMapper;
+import cn.com.itoken.service.admin.domain.TbSysUser;
+
+public interface TbSysUserMapper extends MyMapper<TbSysUser> {
+}
