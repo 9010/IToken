3b0858a5fea9ba1d48db7e0ad77708e4c3c68fc1
Prime <1106690435@qq.com>
2019-08-19 16:30:12 +0800

    添加web admin 和 common web两个module

---
+++ itoken-web-admin/src/main/java/cn/com/self/web/admin/ItokenWebAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.com.self.web.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenWebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenWebAdminApplication.class, args);
+    }
+
+}
