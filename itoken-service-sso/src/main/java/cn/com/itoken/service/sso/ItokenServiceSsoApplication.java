cb9364d29eb5af2ffada8a05724136b2df9663c5
newbone <1106690435@qq.com>
2019-08-22 11:45:37 +0800

    建立SSO Service

---
+++ itoken-service-sso/src/main/java/cn/com/itoken/service/sso/ItokenServiceSsoApplication.java
@@ -0,0 +1,13 @@
+package cn.com.itoken.service.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenServiceSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenServiceSsoApplication.class, args);
+    }
+
+}
