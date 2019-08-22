4b3a0423e6d78946d4d326df43d78166e36c0abd
newbone <1106690435@qq.com>
2019-08-22 15:07:00 +0800

    sso登录controller

--- itoken-service-sso/src/main/java/cn/com/itoken/service/sso/controller/LoginController.java
+++ itoken-service-sso/src/main/java/cn/com/itoken/service/sso/controller/LoginController.java
@@ -1,8 +1,33 @@
 package cn.com.itoken.service.sso.controller;
 
+import cn.com.itoken.service.sso.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
+    @Autowired
+    private LoginService loginService;
 
+    /**
+     * 跳转登录页面
+     * @return
+     */
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String login(){
+        return "login";
+    }
+
+    /**
+     * 登录业务
+     * @param loginCode
+     * @param password
+     * @return
+     */
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String login(String loginCode, String password){
+        return null;
+    }
 }
