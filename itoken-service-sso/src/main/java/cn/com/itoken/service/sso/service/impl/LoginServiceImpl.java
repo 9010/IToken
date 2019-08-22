bd289eaed00c86a22712306d69ff00d91551da4b
newbone <1106690435@qq.com>
2019-08-22 15:02:31 +0800

    增加页面，thymeleaf依赖，配置

--- itoken-service-sso/src/main/java/cn/com/itoken/service/sso/service/impl/LoginServiceImpl.java
+++ itoken-service-sso/src/main/java/cn/com/itoken/service/sso/service/impl/LoginServiceImpl.java
@@ -3,6 +3,8 @@ package cn.com.itoken.service.sso.service.impl;
 import cn.com.itoken.common.domain.TbSysUser;
 import cn.com.itoken.service.sso.mapper.TbSysUserMapper;
 import cn.com.itoken.service.sso.service.LoginService;
+import cn.com.itoken.service.sso.service.consumer.RedisService;
+import cn.com.self.itoken.common.utils.MapperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
@@ -12,18 +14,45 @@ import tk.mybatis.mapper.entity.Example;
 public class LoginServiceImpl implements LoginService {
 
     @Autowired
+    private RedisService redisService;
+
+    @Autowired
     private TbSysUserMapper tbSysUserMapper;
 
     @Override
     public TbSysUser login(String loginCode, String plantPassword) {
-        Example example = new Example(TbSysUser.class);
-        example.createCriteria().andEqualTo("loginCode", loginCode);
+        TbSysUser tbSysUser = null;
 
-        TbSysUser tbSysUser = tbSysUserMapper.selectOneByExample(example);
-        String checkPassword = DigestUtils.md5DigestAsHex(plantPassword.getBytes());
-        if(checkPassword.equals(tbSysUser.getPassword())){
-            return tbSysUser;
+        String json = redisService.get(loginCode);
+
+        //用户信息不存在redis中
+        if(json == null){
+            Example example = new Example(TbSysUser.class);
+            example.createCriteria().andEqualTo("loginCode", loginCode);
+
+            tbSysUser = tbSysUserMapper.selectOneByExample(example);
+            String checkPassword = DigestUtils.md5DigestAsHex(plantPassword.getBytes());
+            if(checkPassword.equals(tbSysUser.getPassword())){
+                try {
+                    redisService.put(loginCode, MapperUtils.obj2json(tbSysUser), 60 * 60 * 24);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return tbSysUser;
+            }
+            else {
+                return null;
+            }
         }
-        return null;
+        //redis中存在
+        else {
+            try {
+                tbSysUser = MapperUtils.json2pojo(json, TbSysUser.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return tbSysUser;
     }
 }
