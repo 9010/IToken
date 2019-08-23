13c925ff7a4ff24fed02dacce711cc1efdd06bda
newbone <1106690435@qq.com>
2019-08-23 10:10:08 +0800

    sso改动

--- itoken-service-sso/src/main/java/cn/com/itoken/service/sso/controller/LoginController.java
+++ itoken-service-sso/src/main/java/cn/com/itoken/service/sso/controller/LoginController.java
@@ -2,16 +2,23 @@ package cn.com.itoken.service.sso.controller;
 
 import cn.com.itoken.common.domain.TbSysUser;
 import cn.com.itoken.service.sso.service.LoginService;
+import cn.com.itoken.service.sso.service.consumer.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.UUID;
+
 @Controller
 public class LoginController {
+
     @Autowired
     private LoginService loginService;
+
+    @Autowired
+    private RedisService redisService;
 
     /**
      * 跳转登录页面
@@ -32,6 +39,12 @@ public class LoginController {
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public String login(String loginCode, String password){
         TbSysUser tbSysUser = loginService.login(loginCode, password);
+
+        if(tbSysUser != null){
+            String token = UUID.randomUUID().toString();
+            redisService.put("token", token, 60 * 60 * 24);
+        }
+
         System.out.println(tbSysUser);
         return "ok";
     }
