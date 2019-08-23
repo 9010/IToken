57b98987ba91f72a43a66bce177b6ae60985e720
newbone <1106690435@qq.com>
2019-08-23 10:21:50 +0800

    token存入cookie

--- itoken-service-sso/src/main/java/cn/com/itoken/service/sso/controller/LoginController.java
+++ itoken-service-sso/src/main/java/cn/com/itoken/service/sso/controller/LoginController.java
@@ -3,12 +3,15 @@ package cn.com.itoken.service.sso.controller;
 import cn.com.itoken.common.domain.TbSysUser;
 import cn.com.itoken.service.sso.service.LoginService;
 import cn.com.itoken.service.sso.service.consumer.RedisService;
+import cn.com.self.itoken.common.utils.CookieUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -37,12 +40,15 @@ public class LoginController {
      */
     @ResponseBody
     @RequestMapping(value = "login", method = RequestMethod.POST)
-    public String login(String loginCode, String password){
+    public String login(String loginCode, String password, String url,
+                        HttpServletRequest request, HttpServletResponse response){
         TbSysUser tbSysUser = loginService.login(loginCode, password);
 
         if(tbSysUser != null){
             String token = UUID.randomUUID().toString();
-            redisService.put("token", token, 60 * 60 * 24);
+            redisService.put(token, loginCode, 60 * 60 * 24);
+            CookieUtils.setCookie(request, response, "token", token);
+            return "redirect" + url;
         }
 
         System.out.println(tbSysUser);
