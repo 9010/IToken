0024fa235fda869836ee4e76bf260a5f43460126
newbone <1106690435@qq.com>
2019-08-26 10:03:33 +0800

    创建初始化常量拦截器

---
+++ itoken-common-web/src/main/java/cn/com/self/common/web/inetceptor/ConstantsInterceptor.java
@@ -0,0 +1,29 @@
+package cn.com.self.common.web.inetceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 初始化常量拦截器
+ */
+public class ConstantsInterceptor implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
