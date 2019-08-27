1a0df94f4b1808f891e25911feb82f4eb2b500e5
newbone <1106690435@qq.com>
2019-08-27 08:52:38 +0800

    拦截器配置文件

--- itoken-common-web/src/main/java/cn/com/self/common/web/config/InterceptorConfig.java
+++ itoken-common-web/src/main/java/cn/com/self/common/web/config/InterceptorConfig.java
@@ -11,5 +11,6 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new ConstantsInterceptor()).addPathPatterns("/**");
+        
     }
 }
