195b9bfeb8f98c5ea486bd63566ae8855e058880
newbone <1106690435@qq.com>
2019-08-28 14:30:04 +0800

    新建itoken-service-posts文件服务提供者

---
+++ itoken-service-posts/src/main/java/cn/com/itoken/service/posts/ItokenServicePostsApplication.java
@@ -0,0 +1,13 @@
+package cn.com.itoken.service.posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenServicePostsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenServicePostsApplication.class, args);
+    }
+
+}
