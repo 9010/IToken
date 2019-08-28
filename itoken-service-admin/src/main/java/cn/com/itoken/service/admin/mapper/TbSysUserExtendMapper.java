9579b8ea2fa774bdea2d717c90dda06772581ec6
newbone <1106690435@qq.com>
2019-08-28 10:24:45 +0800

    建立扩展Mapper，针对特殊业务需求

---
+++ itoken-service-admin/src/main/java/cn/com/itoken/service/admin/mapper/TbSysUserExtendMapper.java
@@ -0,0 +1,7 @@
+package cn.com.itoken.service.admin.mapper;
+
+import cn.com.itoken.common.service.domain.TbSysUser;
+import cn.com.itoken.common.service.tk.mybatis.mapper.MyMapper;
+
+public interface TbSysUserExtendMapper extends MyMapper<TbSysUser> {
+}
