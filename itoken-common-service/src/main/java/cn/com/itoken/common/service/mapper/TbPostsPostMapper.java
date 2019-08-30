6dc84d486d3e56a8368d9ceb1f846a18b0eeb4dd
newbone <1106690435@qq.com>
2019-08-30 16:49:19 +0800

    添加cacheNamespace

--- itoken-common-service/src/main/java/cn/com/itoken/common/service/mapper/TbPostsPostMapper.java
+++ itoken-common-service/src/main/java/cn/com/itoken/common/service/mapper/TbPostsPostMapper.java
@@ -2,6 +2,9 @@ package cn.com.itoken.common.service.mapper;
 
 import cn.com.itoken.common.service.domain.TbPostsPost;
 import cn.com.itoken.common.service.tk.mybatis.mapper.MyMapper;
+import cn.com.itoken.common.service.utils.RedisCache;
+import org.apache.ibatis.annotations.CacheNamespace;
 
+@CacheNamespace(implementation = RedisCache.class)
 public interface TbPostsPostMapper extends MyMapper<TbPostsPost> {
 }
