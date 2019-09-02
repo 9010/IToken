5ec142fb1cdce82bf1f1436d8d3224d1b2620e84
newbone <1106690435@qq.com>
2019-09-02 11:07:28 +0800

    通过测试

--- itoken-service-posts/src/main/java/cn/com/itoken/service/posts/mapper/TbPostsPostExtendMapper.java
+++ itoken-service-posts/src/main/java/cn/com/itoken/service/posts/mapper/TbPostsPostExtendMapper.java
@@ -2,6 +2,10 @@ package cn.com.itoken.service.posts.mapper;
 
 import cn.com.itoken.common.service.domain.TbPostsPost;
 import cn.com.itoken.common.service.tk.mybatis.mapper.MyMapper;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
 
+@Primary
+@Repository
 public interface TbPostsPostExtendMapper extends MyMapper<TbPostsPost> {
 }
