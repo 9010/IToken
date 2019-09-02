a6214f90e9f13ee4ce5fa4ba1286dcbfa92682c9
newbone <1106690435@qq.com>
2019-09-02 11:46:53 +0800

    新建跑文章服务消费者

---
+++ itoken-web-posts/src/main/java/cn/com/itoken/web/posts/ItokenWebPostsApplication.java
@@ -0,0 +1,13 @@
+package cn.com.itoken.web.posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenWebPostsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenWebPostsApplication.class, args);
+    }
+
+}
