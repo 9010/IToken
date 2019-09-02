d53392a54e8aeefd18a597269dd12119a449bad7
newbone <1106690435@qq.com>
2019-09-02 11:49:26 +0800

    初始化文章服务消费者

--- itoken-web-posts/src/main/java/cn/com/itoken/web/posts/ItokenWebPostsApplication.java
+++ itoken-web-posts/src/main/java/cn/com/itoken/web/posts/ItokenWebPostsApplication.java
@@ -2,8 +2,12 @@ package cn.com.itoken.web.posts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "cn.com.itoken")
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ItokenWebPostsApplication {
 
     public static void main(String[] args) {
