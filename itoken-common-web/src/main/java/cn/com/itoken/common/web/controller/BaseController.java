889a482bf900f206ec6890a56739e1e3b994dfb0
newbone <1106690435@qq.com>
2019-09-02 17:29:07 +0800

    BaseController通用消费者控制类

--- itoken-common-web/src/main/java/cn/com/itoken/common/web/controller/BaseController.java
+++ itoken-common-web/src/main/java/cn/com/itoken/common/web/controller/BaseController.java
@@ -1,8 +1,16 @@
 package cn.com.itoken.common.web.controller;
 
+import cn.com.itoken.common.dto.DataTablesResult;
 import cn.com.itoken.common.service.domain.BaseDomain;
+import cn.com.itoken.common.utils.MapperUtils;
 import cn.com.itoken.common.web.service.BaseClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
 public abstract class BaseController<T extends BaseDomain, S extends BaseClientService> {
 
     /**
@@ -11,4 +19,29 @@ public abstract class BaseController<T extends BaseDomain, S extends BaseClientS
     @Autowired
     protected S service;
 
+    @ResponseBody
+    @RequestMapping(value = "page", method = RequestMethod.GET)
+    public DataTablesResult page(HttpServletRequest request){
+        String strDraw = request.getParameter("draw");
+        String strStart = request.getParameter("start");
+        String strLength = request.getParameter("length");
+
+        int draw = strDraw == null ? 0 : Integer.parseInt(strDraw);
+        int start = strDraw == null ? 0 : Integer.parseInt(strStart);
+        int length = strDraw == null ? 10 : Integer.parseInt(strLength);
+
+        String json = service.page(start, length, null);
+        DataTablesResult dataTablesResult = null;
+        try {
+            dataTablesResult = MapperUtils.json2pojo(json, DataTablesResult.class);
+            dataTablesResult.setDraw(draw);
+            dataTablesResult.setRecordsTotal(dataTablesResult.getCursor().getTotal());
+            dataTablesResult.setRecordsFiltered(dataTablesResult.getCursor().getTotal());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return dataTablesResult;
+    }
 }
