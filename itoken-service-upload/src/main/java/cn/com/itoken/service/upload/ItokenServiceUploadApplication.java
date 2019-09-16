5fb2c55e20f072dde7eceee78148b03a2b036f39
newbone <1106690435@qq.com>
2019-09-16 10:37:15 +0800

    新建upload服务

---
+++ itoken-service-upload/src/main/java/cn/com/itoken/service/upload/ItokenServiceUploadApplication.java
@@ -0,0 +1,13 @@
+package cn.com.itoken.service.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItokenServiceUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItokenServiceUploadApplication.class, args);
+    }
+
+}
